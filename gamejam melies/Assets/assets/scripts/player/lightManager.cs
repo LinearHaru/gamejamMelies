2c45897e1c0b05c46ecad7948b3ba24e1ef4de34
LinearHaru <S2Haru@outlook.outlook.com>
2021-06-28 09:15:50 -0300

    luz adicionada ao personagem
    quando o personagem está no chão, ele emite luz para o chão

---
+++ gamejam melies/Assets/assets/scripts/player/lightManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class lightManager : MonoBehaviour
+{
+    private playerMovement player;
+    [SerializeField] Light2D light;
+    private float onIntensity;
+    public float smooth = 10;
+    void Start()
+    {
+        player = GetComponent<playerMovement>();
+        onIntensity = light.intensity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.onGround)
+            light.intensity = Mathf.Lerp(light.intensity, onIntensity, Time.deltaTime * smooth);
+        else
+            light.intensity = Mathf.Lerp(light.intensity, 0, Time.deltaTime * smooth);
+
+    }
+}
